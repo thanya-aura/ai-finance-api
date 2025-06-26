dada24d659acdc59c6668dcc2a6a46606e3dc9e1
thanya-aura <thanyaaura@gmail.com>
2025-06-26 20:09:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
 import pandas as pd
-from processor import analyze_cashflow
+from io import BytesIO
+from utils import analyze_cashflow
 
 app = FastAPI()
 
-@app.post("/analyze/")
+@app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
-    df = pd.read_excel(file.file)
+    contents = await file.read()
+    df = pd.read_excel(BytesIO(contents))
     result = analyze_cashflow(df)
-    return result.to_dict(orient="records")
-
-@app.get("/download-template/")
-async def download_template():
-    return FileResponse(
-        path="sample_cf.xlsx",
-        media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        filename="sample_cf.xlsx"
-    )
+    return result
